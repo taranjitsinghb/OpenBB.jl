d6d15c1f23aab8384708b89538d908dbab51ddb8
massimo.demauri@kuleuven.be <massimo.demauri@kuleuven.be>
2019-08-27 14:52:38 +0200

    Missing definitions

--- src/branch_and_bound/types/BBworkspace.jl
+++ src/branch_and_bound/types/BBworkspace.jl
@@ -3,7 +3,7 @@
 # @Email:  massimo.demauri@gmail.com
 # @Filename: BBworkspace.jl
 # @Last modified by:   massimo
-# @Last modified time: 2019-06-17T16:40:31+02:00
+# @Last modified time: 2019-08-27T14:49:57+02:00
 # @License: LGPL-3.0
 # @Copyright: {{copyright}}
 
@@ -28,4 +28,30 @@ mutable struct BBworkspace{T1<:AbstractWorkspace,T2<:AbstractSharedMemory} <: Ab
     sharedMemory::T2
     # user settings
     settings::BBsettings
+end
+
+
+
+# construct the root node for a problem of the given dimensions
+function BBroot(workspace::BBworkspace)::BBnode
+    numVars = get_numVariables(workspace)
+    numCnss = get_numConstraints(workspace)
+    varBounds = get_variableBounds(workspace)
+    cnsBounds = get_constraintBounds(workspace)
+
+    return BBnode(copy(varBounds[1]),copy(varBounds[2]),
+                  copy(cnsBounds[1]),copy(cnsBounds[2]),
+                  zeros(numVars),zeros(numVars),zeros(numCnss))
+end
+
+# construct the root node for a problem of the given dimensions (with primal initialization)
+function BBroot(workspace::BBworkspace,primal::Array{Float64,1})::BBnode
+    numVars = get_numVariables(workspace)
+    numCnss = get_numConstraints(workspace)
+    varBounds = get_variableBounds(workspace)
+    cnsBounds = get_constraintBounds(workspace)
+
+    return BBnode(copy(varBounds[1]),copy(varBounds[2]),
+                  copy(cnsBounds[1]),copy(cnsBounds[2]),
+                  copy(primal),zeros(numVars),zeros(numCnss))
 end
